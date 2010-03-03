33ea6f9a6668c4f991d378bc5778f160ab38aba1
yanivinbar@gmail.com <yanivinbar@gmail.com@e94e9296-bd2c-0410-aafd-b12fd6e877d6>
2010-03-03 17:39:21 +0000

    DateTimeTest and testing infrastructure

--- branches/2/src/com/google/api/data/client/v2/DateTime.java
+++ branches/2/src/com/google/api/data/client/v2/DateTime.java
@@ -2,11 +2,9 @@
 
 package com.google.api.data.client.v2;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.Locale;
 import java.util.TimeZone;
 
 /**
@@ -15,15 +13,7 @@ import java.util.TimeZone;
  */
 public class DateTime {
 
-  /** RFC 822 date/time format. */
-  private static final SimpleDateFormat dateTimeFormat822 =
-      new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
-
   private static final TimeZone GMT = TimeZone.getTimeZone("GMT");
-
-  static {
-    dateTimeFormat822.setTimeZone(GMT);
-  }
 
   /**
    * Date/time value expressed as the number of ms since the Unix epoch.
@@ -57,6 +47,10 @@ public class DateTime {
     this(value.getTime());
   }
 
+  public DateTime(long value, Integer tzShift) {
+    this(false, value, tzShift);
+  }
+
   public DateTime(boolean dateOnly, long value, Integer tzShift) {
     this.dateOnly = false;
     this.value = value;
@@ -70,10 +64,10 @@ public class DateTime {
 
     Calendar dateTime = new GregorianCalendar(GMT);
     long localTime = this.value;
-    if (this.tzShift != null) {
-      localTime += this.tzShift.longValue() * 60000;
+    Integer tzShift = this.tzShift;
+    if (tzShift != null) {
+      localTime += tzShift.longValue() * 60000;
     }
-
     dateTime.setTimeInMillis(localTime);
 
     appendInt(sb, dateTime.get(Calendar.YEAR), 4);
@@ -82,7 +76,7 @@ public class DateTime {
     sb.append('-');
     appendInt(sb, dateTime.get(Calendar.DAY_OF_MONTH), 2);
 
-    if (!dateOnly) {
+    if (!this.dateOnly) {
 
       sb.append('T');
       appendInt(sb, dateTime.get(Calendar.HOUR_OF_DAY), 2);
@@ -126,63 +120,71 @@ public class DateTime {
 
   @Override
   public String toString() {
-    return toStringRfc822();
+    return toStringRfc3339();
   }
 
-  /** Formats the value as an RFC 822 date/time. */
-  public String toStringRfc822() {
-    if (this.dateOnly) {
-      throw new IllegalArgumentException();
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) {
+      return true;
     }
-    return dateTimeFormat822.format(this.value);
+    if (!(o instanceof DateTime)) {
+      return false;
+    }
+    DateTime other = (DateTime) o;
+    return this.dateOnly == other.dateOnly && this.value == other.value;
   }
 
   /**
    * Parses an RFC 3339 date/time value.
    */
   public static DateTime parseRfc3339(String str) throws NumberFormatException {
-    Calendar dateTime = new GregorianCalendar(GMT);
-    int year = Integer.parseInt(str.substring(0, 4));
-    int month = Integer.parseInt(str.substring(5, 7)) - 1;
-    int day = Integer.parseInt(str.substring(8, 10));
-    int tzIndex;
-    int length = str.length();
-    boolean dateOnly =
-        length <= 10 || Character.toUpperCase(str.charAt(10)) != 'T';
-    if (dateOnly) {
-      dateTime.set(year, month, day);
-      tzIndex = 10;
-    } else {
-      int hourOfDay = Integer.parseInt(str.substring(11, 13));
-      int minute = Integer.parseInt(str.substring(14, 16));
-      int second = Integer.parseInt(str.substring(17, 19));
-      dateTime.set(year, month, day, hourOfDay, minute, second);
-      if (str.charAt(19) == '.') {
-        int milliseconds = Integer.parseInt(str.substring(20, 23));
-        dateTime.set(Calendar.MILLISECOND, milliseconds);
-        tzIndex = 23;
+    try {
+      Calendar dateTime = new GregorianCalendar(GMT);
+      int year = Integer.parseInt(str.substring(0, 4));
+      int month = Integer.parseInt(str.substring(5, 7)) - 1;
+      int day = Integer.parseInt(str.substring(8, 10));
+      int tzIndex;
+      int length = str.length();
+      boolean dateOnly =
+          length <= 10 || Character.toUpperCase(str.charAt(10)) != 'T';
+      if (dateOnly) {
+        dateTime.set(year, month, day);
+        tzIndex = 10;
       } else {
-        tzIndex = 19;
-      }
-    }
-    Integer tzShiftInteger = null;
-    long value = dateTime.getTimeInMillis();
-    if (length > tzIndex) {
-      int tzShift;
-      if (Character.toUpperCase(str.charAt(tzIndex)) == 'Z') {
-        tzShift = 0;
-      } else {
-        tzShift =
-            Integer.parseInt(str.substring(tzIndex + 1, tzIndex + 3)) * 60
-                + Integer.parseInt(str.substring(tzIndex + 4, tzIndex + 6));
-        if (str.charAt(tzIndex) == '-') {
-          tzShift = -tzShift;
+        int hourOfDay = Integer.parseInt(str.substring(11, 13));
+        int minute = Integer.parseInt(str.substring(14, 16));
+        int second = Integer.parseInt(str.substring(17, 19));
+        dateTime.set(year, month, day, hourOfDay, minute, second);
+        if (str.charAt(19) == '.') {
+          int milliseconds = Integer.parseInt(str.substring(20, 23));
+          dateTime.set(Calendar.MILLISECOND, milliseconds);
+          tzIndex = 23;
+        } else {
+          tzIndex = 19;
         }
-        value -= tzShift * 60000;
       }
-      tzShiftInteger = tzShift;
+      Integer tzShiftInteger = null;
+      long value = dateTime.getTimeInMillis();
+      if (length > tzIndex) {
+        int tzShift;
+        if (Character.toUpperCase(str.charAt(tzIndex)) == 'Z') {
+          tzShift = 0;
+        } else {
+          tzShift =
+              Integer.parseInt(str.substring(tzIndex + 1, tzIndex + 3)) * 60
+                  + Integer.parseInt(str.substring(tzIndex + 4, tzIndex + 6));
+          if (str.charAt(tzIndex) == '-') {
+            tzShift = -tzShift;
+          }
+          value -= tzShift * 60000;
+        }
+        tzShiftInteger = tzShift;
+      }
+      return new DateTime(dateOnly, value, tzShiftInteger);
+    } catch (StringIndexOutOfBoundsException e) {
+      throw new NumberFormatException("Invalid date/time format.");
     }
-    return new DateTime(dateOnly, value, tzShiftInteger);
   }
 
   /** Appends a zero-padded number to a string builder. */
