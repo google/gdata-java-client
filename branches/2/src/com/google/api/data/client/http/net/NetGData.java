e06ac220375c30fd44f142b2fd6d366c381dd0cd
yanivinbar@gmail.com <yanivinbar@gmail.com@e94e9296-bd2c-0410-aafd-b12fd6e877d6>
2010-03-10 19:18:16 +0000

    restructure samples and main project as separate Eclipse projects (part 2)

--- branches/2/src/com/google/api/data/client/http/net/NetGData.java
+++
@@ -1,7 +0,0 @@
-// Copyright 2010 Google Inc. All Rights Reserved.
-
-package com.google.api.data.client.http.net;
-
-public final class NetGData {
-  public static final NetHttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-}
