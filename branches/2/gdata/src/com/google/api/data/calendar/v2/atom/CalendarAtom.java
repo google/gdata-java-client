8be7752b69824b79a4c79007a3db4f686cd0bf8e
yanivinbar@gmail.com <yanivinbar@gmail.com@e94e9296-bd2c-0410-aafd-b12fd6e877d6>
2010-03-15 20:25:54 +0000

    add Calendar and Maps layers

---
+++ branches/2/gdata/src/com/google/api/data/calendar/v2/atom/CalendarAtom.java
@@ -0,0 +1,23 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+
+package com.google.api.data.calendar.v2.atom;
+
+import com.google.api.data.client.v2.atom.NamespaceDictionary;
+
+public final class CalendarAtom {
+
+  public static final NamespaceDictionary NAMESPACE_DICTIONARY;
+  static {
+    NamespaceDictionary.Builder builder = new NamespaceDictionary.Builder();
+    builder.addNamespace("", "http://www.w3.org/2005/Atom");
+    builder.addNamespace("app", "http://www.w3.org/2007/app");
+    builder.addNamespace("gAcl", "http://schemas.google.com/acl/2007");
+    builder.addNamespace("gCal", "http://schemas.google.com/gCal/2005");
+    builder.addNamespace("gd", "http://schemas.google.com/g/2005");
+    builder.addNamespace("openSearch", "http://a9.com/-/spec/opensearch/1.1/");
+    NAMESPACE_DICTIONARY = builder.build();
+  }
+
+  private CalendarAtom() {
+  }
+}
