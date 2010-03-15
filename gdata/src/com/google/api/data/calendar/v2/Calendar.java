a17be61a37945fc1e4cd68313ac2f11f15fdeeb2
yanivinbar@gmail.com <yanivinbar@gmail.com@e94e9296-bd2c-0410-aafd-b12fd6e877d6>
2010-03-15 20:25:54 +0000

    add Calendar and Maps layers

---
+++ gdata/src/com/google/api/data/calendar/v2/Calendar.java
@@ -0,0 +1,13 @@
+// Copyright 2010 Google Inc. All Rights Reserved.
+
+package com.google.api.data.calendar.v2;
+
+public final class Calendar {
+
+  public static final String AUTH_TOKEN_TYPE = "cl";
+
+  public static final String VERSION = "2";
+
+  private Calendar() {
+  }
+}
