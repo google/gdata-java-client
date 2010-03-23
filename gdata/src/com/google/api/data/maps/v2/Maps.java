85b66a67aff8fd8c93760957fe34b9abaa720f8e
yanivinbar@gmail.com <yanivinbar@gmail.com@e94e9296-bd2c-0410-aafd-b12fd6e877d6>
2010-03-23 20:20:57 +0000

    Better packaging of the distribution zip file, by packaging the jars for Android under an "android" directory, more fine-grained control for splitting the jars, and code generation of each API-specific package.

--- gdata/src/com/google/api/data/maps/v2/Maps.java
+++ gdata/src/com/google/api/data/maps/v2/Maps.java
@@ -2,10 +2,15 @@
 
 package com.google.api.data.maps.v2;
 
+/** Constants for the Google Maps Data API. */
 public final class Maps {
 
+/** The authentication token type or service name. */
   public static final String AUTH_TOKEN_TYPE = "local";
 
+/** Version name. */
   public static final String VERSION = "2";
 
+  private Maps() {
+  }
 }
--- gdata/src/com/google/api/data/youtube/v2/YouTube.java
+++ gdata/src/com/google/api/data/youtube/v2/YouTube.java
@@ -2,10 +2,15 @@
 
 package com.google.api.data.youtube.v2;
 
+/** Constants for the YouTube Data API. */
 public final class YouTube {
 
+/** The authentication token type or service name. */
   public static final String AUTH_TOKEN_TYPE = "youtube";
 
+/** Version name. */
   public static final String VERSION = "2";
 
+  private YouTube() {
+  }
 }
