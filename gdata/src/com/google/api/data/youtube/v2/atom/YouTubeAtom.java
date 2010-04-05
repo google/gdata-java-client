4a0627498d4549ce3b0746dee111bf8a844b519a
yanivinbar@gmail.com <yanivinbar@gmail.com@e94e9296-bd2c-0410-aafd-b12fd6e877d6>
2010-04-05 13:58:58 +0000

    Major features: package renamings, ArrayMap, Entity extends AbstractMap, ReflectionMap, fix file headers for Apache License, ClientLogin and AuthSub authentication, OAuth WRAP WebAppProfile, GoogleTransport, UrlEncodedFormHttpParser, splitting generic XML and JSON parts from Googel-specific parts

--- gdata/src/com/google/api/data/youtube/v2/atom/YouTubeAtom.java
+++ gdata/src/com/google/api/data/youtube/v2/atom/YouTubeAtom.java
@@ -1,26 +1,41 @@
-// Copyright 2010 Google Inc. All Rights Reserved.
+/* Copyright (c) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.google.api.data.youtube.v2.atom;
 
-import com.google.api.data.client.v2.atom.NamespaceDictionary;
+import com.google.api.client.xml.XmlNamespaceDictionary;
+
+import java.util.Map;
 
 public final class YouTubeAtom {
 
-  public static final NamespaceDictionary NAMESPACE_DICTIONARY;
+  public static final XmlNamespaceDictionary NAMESPACE_DICTIONARY = new
+      XmlNamespaceDictionary();
   static {
-    NamespaceDictionary.Builder builder = new NamespaceDictionary.Builder();
-    builder.addNamespace("", "http://www.w3.org/2005/Atom");
-    builder.addNamespace("app", "http://www.w3.org/2007/app");
-    builder.addNamespace("atom", "http://www.w3.org/2005/Atom");
-    builder.addNamespace("batch", "http://schemas.google.com/gdata/batch");
-    builder.addNamespace("gd", "http://schemas.google.com/g/2005");
-    builder.addNamespace("georss", "http://www.georss.org/georss");
-    builder.addNamespace("gml", "http://www.opengis.net/gml");
-    builder.addNamespace("media", "http://search.yahoo.com/mrss/");
-    builder.addNamespace("openSearch", "http://a9.com/-/spec/opensearch/1.1/");
-    builder.addNamespace("xml", "http://www.w3.org/XML/1998/namespace");
-    builder.addNamespace("yt", "http://gdata.youtube.com/schemas/2007");
-    NAMESPACE_DICTIONARY = builder.build();
+    Map<String, String> map = NAMESPACE_DICTIONARY.namespaceAliasToUriMap;
+    map.put("", "http://www.w3.org/2005/Atom");
+    map.put("app", "http://www.w3.org/2007/app");
+    map.put("atom", "http://www.w3.org/2005/Atom");
+    map.put("batch", "http://schemas.google.com/gdata/batch");
+    map.put("gd", "http://schemas.google.com/g/2005");
+    map.put("georss", "http://www.georss.org/georss");
+    map.put("gml", "http://www.opengis.net/gml");
+    map.put("media", "http://search.yahoo.com/mrss/");
+    map.put("openSearch", "http://a9.com/-/spec/opensearch/1.1/");
+    map.put("xml", "http://www.w3.org/XML/1998/namespace");
+    map.put("yt", "http://gdata.youtube.com/schemas/2007");
   }
 
   private YouTubeAtom() {
