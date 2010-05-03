62a4cb9c21717a171f15ecc7fabc465871824c0a
yanivinbar@gmail.com <yanivinbar@gmail.com@e94e9296-bd2c-0410-aafd-b12fd6e877d6>
2010-05-03 20:28:58 +0000

    OAuth 1.0a, package renaming, bug fixes, and lots of other minor improvements

---
+++ branches/2/gdata/src/com/google/api/client/auth/wrap/UsernameAndPasswordProfile.java
@@ -0,0 +1,42 @@
+package com.google.api.client.http.auth.wrap;
+
+import com.google.api.client.Name;
+import com.google.api.client.http.UriEntity;
+
+/**
+ * Implements the Username and Password Profile of OAuth WRAP authorization as
+ * specified in <a
+ * href="http://tools.ietf.org/html/draft-hardt-oauth-01#section-6.1">OAuth Web
+ * Resource Authorization Profiles</a>.
+ */
+public class UsernameAndPasswordProfile {
+
+  public static class AccessTokenRequestUriEntity extends UriEntity {
+
+    /** The Client Identifier (required). */
+    @Name("wrap_client_id")
+    public String clientId;
+
+    /** The User's username (required). */
+    @Name("wrap_username")
+    public String username;
+
+    /** The User's password (required). */
+    @Name("wrap_password")
+    public String password;
+
+    /**
+     * The Authorization Server MAY define authorization scope values for the
+     * Client to include (optional).
+     */
+    @Name("wrap_scope")
+    public String scope;
+
+    public AccessTokenRequestUriEntity(String uri) {
+      super(uri);
+    }
+  }
+
+  private UsernameAndPasswordProfile() {
+  }
+}
