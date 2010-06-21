67452d146b0a1af01ceeeeeaa4d7d76f0c69e7e1
yanivinbar@gmail.com <yanivinbar@gmail.com@e94e9296-bd2c-0410-aafd-b12fd6e877d6>
2010-06-21 16:45:01 +0000

    Deprecate com.google.api.data.* packages and remove anything that wasn't in version 2.2

--- gdata/src/com/google/api/data/books/v1/GoogleBookSearch.java
+++ gdata/src/com/google/api/data/books/v1/GoogleBookSearch.java
@@ -20,16 +20,37 @@ package com.google.api.data.books.v1;
  * Constants for the Google Book Search Data API.
  *
  * @since 2.2
+ * @deprecated (scheduled to be removed in version 2.4) Copy into your own
+ * application
  */
+@Deprecated
 public final class GoogleBookSearch {
 
-  /** Version name. */
+  /**
+   * Version name.
+   *
+   * @deprecated (scheduled to be removed in version 2.4) Copy into your own
+   * application
+   */
+  @Deprecated
   public static final String VERSION = "1";
 
-  /** Root URL. */
+  /**
+   * Root URL.
+   *
+   * @deprecated (scheduled to be removed in version 2.4) Copy into your own
+   * application
+   */
+  @Deprecated
   public static final String ROOT_URL = "http://www.google.com/books/feeds/";
 
-  /** The authentication token type used for Client Login. */
+  /**
+   * The authentication token type used for Client Login.
+   *
+   * @deprecated (scheduled to be removed in version 2.4) Copy into your own
+   * application
+   */
+  @Deprecated
   public static final String AUTH_TOKEN_TYPE = "print";
 
   private GoogleBookSearch() {
