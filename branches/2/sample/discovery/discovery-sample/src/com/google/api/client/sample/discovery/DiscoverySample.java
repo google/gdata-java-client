9fba8d5ceee8a7fc50885cacc2c7053fcf33830c
yanivinbar@gmail.com <yanivinbar@gmail.com@e94e9296-bd2c-0410-aafd-b12fd6e877d6>
2010-07-12 18:42:30 +0000

    Improve discovery sample to be able to call any discovery-based Google API from the command line

--- branches/2/sample/discovery/discovery-sample/src/com/google/api/client/sample/discovery/DiscoverySample.java
+++ branches/2/sample/discovery/discovery-sample/src/com/google/api/client/sample/discovery/DiscoverySample.java
@@ -17,14 +17,31 @@
 package com.google.api.client.sample.discovery;
 
 import com.google.api.client.apache.ApacheHttpTransport;
+import com.google.api.client.auth.oauth.OAuthAuthorizeTemporaryTokenUrl;
+import com.google.api.client.auth.oauth.OAuthCredentialsResponse;
+import com.google.api.client.auth.oauth.OAuthHmacSigner;
+import com.google.api.client.auth.oauth.OAuthParameters;
+import com.google.api.client.googleapis.auth.oauth.GoogleOAuthAuthorizeTemporaryTokenUrl;
+import com.google.api.client.googleapis.auth.oauth.GoogleOAuthGetAccessToken;
+import com.google.api.client.googleapis.auth.oauth.GoogleOAuthGetTemporaryToken;
 import com.google.api.client.googleapis.json.DiscoveryDocument;
 import com.google.api.client.googleapis.json.DiscoveryDocument.ServiceMethod;
 import com.google.api.client.googleapis.json.DiscoveryDocument.ServiceParameter;
 import com.google.api.client.googleapis.json.DiscoveryDocument.ServiceResource;
+import com.google.api.client.googleapis.json.JsonCContent;
+import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponseException;
 import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.InputStreamContent;
 
+import java.awt.Desktop;
+import java.awt.Desktop.Action;
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -34,99 +51,340 @@ import java.util.regex.Pattern;
  */
 public class DiscoverySample {
 
-  private static final Pattern PATTERN =
+  private static final String APP_NAME = "Google Discovery API Client v0.1";
+
+  private static final Pattern API_NAME_PATTERN = Pattern.compile("\\w+");
+
+  private static final Pattern METHOD_PATTERN =
       Pattern.compile("(\\w+)(\\.(\\w+)(\\.(\\w+))?)?");
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
+    // initialize HTTP transport
     HttpTransport.setLowLevelHttpTransport(ApacheHttpTransport.INSTANCE);
     Debug.enableLogging();
-    System.out.println("Discovery v0.1 Sample");
+    // parse command argument
     if (args.length == 0) {
-      System.out.println("Usage: discover [name]");
-      System.out.println();
-      System.out.println("Examples:");
-      System.out.println("  discover buzz");
-      System.out.println("  discover buzz.activities");
-      System.out.println("  discover buzz.activities.list");
+      showMainHelp();
     } else {
-      String param = args[0];
-      Matcher m = PATTERN.matcher(param);
-      if (!m.matches()) {
-        System.err.println("ERROR: invalid input");
+      String command = args[0];
+      if (command.equals("help")) {
+        help(args);
+      } else if (command.equals("call")) {
+        call(args);
+      } else if (command.equals("discover")) {
+        discover(args);
       } else {
-        String api = m.group(1);
-        DiscoveryDocument doc = null;
-        try {
-          doc = DiscoveryDocument.execute(api);
-        } catch (HttpResponseException e) {
-          if (e.response.statusCode == 404) {
-            System.err.println("ERROR: API not found: " + api);
-            e.response.ignore();
-          } else {
-            throw e;
+        error(null, "unknown command: " + command);
+      }
+    }
+  }
+
+  private static void help(String[] args) {
+    if (args.length == 1) {
+      showMainHelp();
+    } else {
+      String helpCommand = args[1];
+      if (helpCommand.equals("call")) {
+        System.out.println("Usage: google call methodName [parameters]");
+        System.out.println();
+        System.out.println("Examples:");
+        System.out.println(
+            "  google call discovery.apis.get --api buzz --prettyprint true");
+        System.out
+            .println(
+                "  google call buzz.activities.list --scope @self --userId @me --alt json --prettyprint true");
+        System.out
+            .println(
+                "  google call buzz.activities.insert --userId @me /tmp/buzzpost.json");
+      } else if (helpCommand.equals("discover")) {
+        System.out.println("Usage: google discover apiName");
+        System.out.println();
+        System.out.println("Examples:");
+        System.out.println("  google discover buzz");
+        System.out.println("  google discover moderator");
+      } else {
+        error(null, "unknown command: " + helpCommand);
+      }
+    }
+  }
+
+  private static void showMainHelp() {
+    System.out.println("Google Discovery API v0.1");
+    System.out.println();
+    System.out.println("For more help on a specific command, type one of:");
+    System.out.println();
+    System.out.println("  google help call");
+    System.out.println("  google help discover");
+  }
+
+  private static void error(String command, String detail) {
+    System.err.println("ERROR: " + detail);
+    System.err.println(
+        "For help, type: google" + (command == null ? "" : " help " + command));
+    System.exit(1);
+  }
+
+  private static void call(String[] args) throws Exception {
+    if (args.length == 1) {
+      error("call", "missing method name");
+    }
+    String fullMethodName = args[1];
+    Matcher m = METHOD_PATTERN.matcher(fullMethodName);
+    if (!m.matches()) {
+      error("call", "invalid method name: " + fullMethodName);
+    }
+    String apiName = m.group(1);
+    String resourceName = m.group(3);
+    String methodName = m.group(5);
+    DiscoveryDocument doc = loadDiscoveryDocument(apiName);
+    Map<String, ServiceResource> resources = doc.serviceDefinition.resources;
+    ServiceMethod method = null;
+    if (resources != null) {
+      ServiceResource resource = resources.get(resourceName);
+      Map<String, ServiceMethod> methods = resource.methods;
+      if (methods != null) {
+        method = methods.get(methodName);
+      }
+    }
+    if (method == null) {
+      error("call", "method not found: " + fullMethodName);
+    }
+    HashMap<String, String> parameters = new HashMap<String, String>();
+    HashMap<String, String> queryParameters = new HashMap<String, String>();
+    File requestBodyFile = null;
+    int i = 2;
+    while (i < args.length) {
+      String argName = args[i++];
+      if (argName.startsWith("--")) {
+        String parameterName = argName.substring(2);
+        if (i == args.length) {
+          error("call", "missing parameter value for: " + argName);
+        }
+        String parameterValue = args[i++];
+        if (method.parameters == null
+            || !method.parameters.containsKey(parameterName)) {
+          queryParameters.put(parameterName, parameterValue);
+        } else {
+          String oldValue = parameters.put(parameterName, parameterValue);
+          if (oldValue != null) {
+            error("call", "duplicate parameter: " + argName);
           }
         }
-        if (doc != null) {
-          String resourceName = m.group(3);
-          if (resourceName == null) {
-            System.out.println("Resources in " + api + ":");
-            String first = null;
-            for (String name : doc.serviceDefinition.resources.keySet()) {
-              if (first == null) {
-                first = name;
-              }
-              System.out.println("  " + name);
-            }
-            System.out.println();
-            System.out.println(
-                "Example to list methods for a resource: discover " + api + "."
-                    + first);
-          } else {
-            ServiceResource resource =
-                doc.serviceDefinition.resources.get(resourceName);
-            String fullResourceName = api + "." + resourceName;
-            if (resource == null) {
-              System.err.println(
-                  "ERROR: Resource not found: " + fullResourceName);
-            } else {
-              String methodName = m.group(5);
-              if (methodName == null) {
-                System.out.println("Methods in " + fullResourceName + ":");
-                String first = null;
-                for (String name : resource.methods.keySet()) {
-                  if (first == null) {
-                    first = name;
-                  }
-                  System.out.println("  " + name);
-                }
-                System.out.println();
-                System.out.println(
-                    "Example to list parameters of a method: discover "
-                        + fullResourceName + "." + first);
-              } else {
-                ServiceMethod method = resource.methods.get(methodName);
-                String fullMethodName = fullResourceName + "." + methodName;
-                if (method == null) {
-                  System.err.println(
-                      "ERROR: Method not found: " + fullMethodName);
+      } else {
+        if (requestBodyFile != null) {
+          error(
+              "call", "multiple HTTP request body files specified: " + argName);
+        }
+        String fileName = argName;
+        requestBodyFile = new File(fileName);
+        if (!requestBodyFile.canRead()) {
+          error("call", "unable to read file: " + argName);
+        }
+      }
+    }
+    for (Map.Entry<String, ServiceParameter> parameter :
+        method.parameters.entrySet()) {
+      String paramName = parameter.getKey();
+      if (parameter.getValue().required && !parameters.containsKey(paramName)) {
+        error("call", "missing required parameter: " + paramName);
+      }
+    }
+    doc.transport = new HttpTransport();
+    HttpRequest request =
+        doc.buildRequest(resourceName + "." + methodName, parameters);
+    request.url.putAll(queryParameters);
+    if (requestBodyFile != null) {
+      JsonCContent content = new JsonCContent();
+      InputStreamContent fileContent = new InputStreamContent();
+      // TODO: support other content types?
+      fileContent.type = "application/json";
+      fileContent.setFileInput(requestBodyFile);
+      request.content = fileContent;
+    }
+    try {
+      authorizeUsingOAuth(doc, apiName, method);
+      String response = request.execute().parseAsString();
+      System.out.println(response);
+    } catch (HttpResponseException e) {
+      System.err.println(e.response.parseAsString());
+      System.exit(1);
+    } finally {
+      if (credentials != null) {
+        try {
+          GoogleOAuthGetAccessToken.revokeAccessToken(createOAuthParameters());
+        } catch (Exception e) {
+          e.printStackTrace(System.err);
+        }
+      }
+    }
+  }
+
+  private static DiscoveryDocument loadDiscoveryDocument(String apiName)
+      throws IOException {
+    try {
+      return DiscoveryDocument.load(apiName);
+    } catch (HttpResponseException e) {
+      if (e.response.statusCode == 404) {
+        error("discover", "API not found: " + apiName);
+      }
+      throw e;
+    }
+  }
+
+  private static void discover(String[] args) throws IOException {
+    System.out.println("Google Discovery API Client v0.1");
+    // load discovery doc
+    if (args.length == 1) {
+      error("discover", "missing api name");
+    }
+    String apiName = args[1];
+    if (!API_NAME_PATTERN.matcher(apiName).matches()) {
+      System.err.println("ERROR: invalid API name: " + apiName);
+    }
+    DiscoveryDocument doc = loadDiscoveryDocument(apiName);
+    // compute method details
+    ArrayList<MethodDetails> result = new ArrayList<MethodDetails>();
+    Map<String, ServiceResource> resources = doc.serviceDefinition.resources;
+    if (resources != null) {
+      for (Map.Entry<String, ServiceResource> resourceEntry :
+          resources.entrySet()) {
+        String resourceName = apiName + "." + resourceEntry.getKey();
+        ServiceResource resource = resourceEntry.getValue();
+        Map<String, ServiceMethod> methods = resource.methods;
+        if (methods != null) {
+          for (Map.Entry<String, ServiceMethod> methodEntry :
+              methods.entrySet()) {
+            MethodDetails details = new MethodDetails();
+            details.name = resourceName + "." + methodEntry.getKey();
+            Map<String, ServiceParameter> parameters =
+                methodEntry.getValue().parameters;
+            if (parameters != null) {
+              for (Map.Entry<String, ServiceParameter> parameterEntry :
+                  parameters.entrySet()) {
+                String parameterName = parameterEntry.getKey();
+                if (parameterEntry.getValue().required) {
+                  details.requiredParameters.add(parameterName);
                 } else {
-                  System.out.println("Parameters for " + fullMethodName + ":");
-                  for (Map.Entry<String, ServiceParameter> entry :
-                      method.parameters.entrySet()) {
-                    String name = entry.getKey();
-                    System.out.print("  " + name);
-                    ServiceParameter parameter = entry.getValue();
-                    if (parameter.required) {
-                      System.out.print(" (required)");
-                    }
-                    System.out.println();
-                  }
+                  details.optionalParameters.add(parameterName);
                 }
               }
+              Collections.sort(details.requiredParameters);
+              Collections.sort(details.optionalParameters);
+              result.add(details);
             }
           }
         }
       }
+    }
+    Collections.sort(result);
+    // display method details
+    for (MethodDetails methodDetail : result) {
+      System.out.println();
+      System.out.println(methodDetail.name);
+      System.out.println("  required parameters: "
+          + showParams(methodDetail.requiredParameters));
+      System.out.println("  optional parameters: "
+          + showParams(methodDetail.optionalParameters));
+    }
+  }
+
+  private static String showParams(ArrayList<String> params) {
+    StringBuilder buf = new StringBuilder();
+    for (int i = 0; i < params.size(); i++) {
+      if (i != 0) {
+        buf.append(", ");
+      }
+      buf.append(params.get(i));
+    }
+    return buf.toString();
+  }
+
+  static OAuthHmacSigner signer;
+  static OAuthCredentialsResponse credentials;
+
+  private static OAuthParameters createOAuthParameters() {
+    OAuthParameters authorizer = new OAuthParameters();
+    authorizer.consumerKey = "anonymous";
+    authorizer.signer = signer;
+    authorizer.token = credentials.token;
+    return authorizer;
+  }
+
+  private static void authorizeUsingOAuth(
+      DiscoveryDocument doc, String apiName, ServiceMethod method)
+      throws Exception {
+    if (apiName.equals("discovery") || apiName.equals("diacritize")) {
+      return;
+    }
+    HttpTransport transport = doc.transport;
+    // callback server
+    LoginCallbackServer callbackServer = null;
+    String verifier = null;
+    String tempToken = null;
+    try {
+      callbackServer = new LoginCallbackServer();
+      callbackServer.start();
+      // temporary token
+      GoogleOAuthGetTemporaryToken temporaryToken =
+          new GoogleOAuthGetTemporaryToken();
+      signer = new OAuthHmacSigner();
+      signer.clientSharedSecret = "anonymous";
+      temporaryToken.signer = signer;
+      temporaryToken.consumerKey = "anonymous";
+      temporaryToken.scope = "https://www.googleapis.com/auth/" + apiName;
+      temporaryToken.displayName = APP_NAME;
+      temporaryToken.callback = callbackServer.getCallbackUrl();
+      OAuthCredentialsResponse tempCredentials = temporaryToken.execute();
+      signer.tokenSharedSecret = tempCredentials.tokenSecret;
+      // authorization URL
+      OAuthAuthorizeTemporaryTokenUrl authorizeUrl;
+      if (apiName.equals("buzz")) {
+        authorizeUrl = new OAuthAuthorizeTemporaryTokenUrl(
+            "https://www.google.com/buzz/api/auth/OAuthAuthorizeToken");
+        authorizeUrl.set("scope", temporaryToken.scope);
+        authorizeUrl.set("domain", "anonymous");
+        authorizeUrl.set("xoauth_displayname", APP_NAME);
+      } else if (apiName.equals("latitude")) {
+        // TODO: test!
+        authorizeUrl = new OAuthAuthorizeTemporaryTokenUrl(
+            "https://www.google.com/latitude/apps/OAuthAuthorizeToken");
+      } else {
+        authorizeUrl = new GoogleOAuthAuthorizeTemporaryTokenUrl();
+      }
+      authorizeUrl.temporaryToken = tempToken = tempCredentials.token;
+      String authorizationUrl = authorizeUrl.build();
+      launchBrowser(authorizationUrl);
+      verifier = callbackServer.waitForVerifier(tempToken);
+    } finally {
+      if (callbackServer != null) {
+        callbackServer.stop();
+      }
+    }
+    GoogleOAuthGetAccessToken accessToken = new GoogleOAuthGetAccessToken();
+    accessToken.temporaryToken = tempToken;
+    accessToken.signer = signer;
+    accessToken.consumerKey = "anonymous";
+    accessToken.verifier = verifier;
+    credentials = accessToken.execute();
+    signer.tokenSharedSecret = credentials.tokenSecret;
+    createOAuthParameters().signRequestsUsingAuthorizationHeader(transport);
+  }
+
+  private static void launchBrowser(String authorizationUrl)
+      throws IOException {
+
+    boolean browsed = false;
+    if (Desktop.isDesktopSupported()) {
+      Desktop desktop = Desktop.getDesktop();
+      if (desktop.isSupported(Action.BROWSE)) {
+        desktop.browse(URI.create(authorizationUrl));
+        browsed = true;
+      }
+    }
+
+    if (!browsed) {
+      String browser = "google-chrome";
+      Runtime.getRuntime().exec(new String[] {browser, authorizationUrl});
     }
   }
 }
